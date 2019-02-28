31db1fa9ae1a00d6fd6309e237f3c8e5706c86a0
olyashepko <olya.martysyuk@gmail.com>
2019-02-28 18:48:15 +0300

    feat: git func, which count amount of love triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let sum=0;
+
+for (let i=0; i<preferences.length;i++) {
+  if (preferences [preferences[preferences[i]-1]-1] == (i+1) && preferences[i]!=(i+1)) {
+    sum=sum+1;
+  }
+}
+return sum/3
 };
